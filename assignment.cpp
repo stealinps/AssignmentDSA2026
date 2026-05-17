bec99872068dceecf532a99b958d7a5f9d146fdd
STEAL <stealinp@gmail.com>
2026-05-17 20:42:20 +0800

    refactor

--- assignment.cpp
+++ assignment.cpp
@@ -1,59 +1,65 @@
 #include <iostream>
-#include "raylib.h"
-Color turqoise={0,255,255,255};
-Color darkred={67,0,0,255};
+#include <raylib.h>
 
-int cellSize=30;
-int cellCount=25;
-Vector2 GenerateRandomPos();
+#define COLS 25
+#define ROWS 25
 
-class Snake{
-	
+enum Tile { FLOOR, WALL };
+
+Tile grid[ROWS][COLS];
+
+void InitGrid() {
+    for (int y = 0; y < ROWS; y++)
+        for (int x = 0; x < COLS; x++)
+            grid[y][x] = FLOOR;
+}
+void DrawGrid(int cellSize) {
+	for (int y = 0; y < ROWS; y++) {
+		for (int x = 0; x < COLS; x++) {
+			Color c;
+			if(grid[y][x]==WALL) c=DARKGRAY;
+			else c=LIGHTGRAY ;
+			DrawRectangle(x * cellSize, y * cellSize, cellSize, cellSize, c);
+			DrawRectangleLines(x * cellSize, y * cellSize, cellSize, cellSize, GRAY);
+        }
+    }
+}
+
+struct Player {
+	int x = 1, y = 1;  // grid coordinates, not pixels
+	void Draw(int cellSize) {
+	DrawRectangle(x * cellSize, y * cellSize, cellSize, cellSize, BLUE);
+}
+	void HandleInput() {
+		if (IsKeyPressed(KEY_W) && y > 0)          y--;
+		if (IsKeyPressed(KEY_S) && y < ROWS - 1)   y++;
+		if (IsKeyPressed(KEY_A) && x > 0)           x--;
+		if (IsKeyPressed(KEY_D) && x < COLS - 1)    x++;
+	}
 };
 
-class Food{
-	public:
-		Food();
-		~Food();
-		Texture2D texture;
-		Vector2 position;
-		void Draw();
 
-	private:
 
-};
-
-Food::Food(){
-	Image image=LoadImage("Graphics/Food.png");
-	texture=LoadTextureFromImage(image);
-	position = GenerateRandomPos();
-	UnloadImage(image);
-}
-Food::~Food(){
-	UnloadTexture(texture);
-}
-void Food::Draw(){
-	DrawTexture(texture,position.x*(10+cellSize),position.y*(10+cellSize),WHITE);
-}
-Vector2 GenerateRandomPos(){
-	float x = GetRandomValue(0,cellCount-1);
-	float y = GetRandomValue(0, cellCount-1);
-	return Vector2{x,y};
-}
-int main() {
-	std::cout<<"Starting Game";
-	InitWindow(cellSize*cellCount,cellSize*cellCount, "Snake Game");
-	Food food;
+int main(){
+	const int cellSize = 30; 
+	Color darkBlue={44,44,127,255};
+	int winwidth=720;
+	int winheight=480;
+	InitWindow(winwidth,winheight,"Project DSA");
 	SetTargetFPS(60);
-	while (!WindowShouldClose()) {
-		BeginDrawing();
-		food.Draw();
-		ClearBackground(darkred);
-		DrawText("WIP", 190, 200, 20, turqoise);
+	Player player;
 
+	InitGrid();
+	grid[5][5] = WALL;  // test wall
+
+	while(WindowShouldClose()==false){
+		player.HandleInput(); 
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawGrid(cellSize);
+		player.Draw(cellSize);
 		EndDrawing();
 	}
 	CloseWindow();
-	return 0;
-}
 
+}
